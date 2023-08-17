252483639bc999389fb22864393dffbd627fd61a
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2023-08-17 15:06:50 +0000

    change colors in card

--- portfolio/components/Card.jsx
+++ portfolio/components/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Card = ({title, image, content, deployed}) => {
   return (
-    <div className='h-auto bg-slate-900 rounded-xl overflow-hidden m-1'>
-      {image && <img src={image} alt={title} className='w-full object-cover' />}
-      <h2 className='text-lime-600 font-bold p-2'>{title}</h2>
-      <p className='text-stone-300 p-2'>{content}</p>
-      <a className='text-stone-400 hover:underline hover:text-lime-600 p-2' href={deployed} rel='noopener noreferrer' target='_blank'>View webiste</a>
+    <div className='h-auto bg-slate-900 rounded-xl overflow-hidden m-1' >
+      {image && <img src={image} alt={title} className='w-full object-cover'/>}
+      <h2 className='text-stone-300 font-bold p-2'>{title}</h2>
+      <p className='text-stone-400 p-2'>{content}</p>
+      <a className='text-lime-600 underline hover:text-lime-400 p-2 ' href={deployed} rel='noopener noreferrer' target='_blank'>View Website Here!</a>
     </div>
   )
 }
