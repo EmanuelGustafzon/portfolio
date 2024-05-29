e45de69e6de601f5251d0509551ff8a9f5aa9ae4
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-29 20:44:45 +0200

    Update Banner.jsx
    Test emoji

--- portfolio/components/Banner.jsx
+++ portfolio/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <Image width="600" height="700" src="/images/switzerland.jpg" className="rounded-lg shadow-2xl" />
                 <div>
-                <h1 className="text-5xl font-bold sm:text-lg">&quot;Hello World!&quot;</h1>
+                <h1 className="text-5xl font-bold sm:text-sm">Hi There 👋</h1>
                 <p className="py-6">My name is Emanuel and I am a passionate coder who love to write logic and deepen my knowledge and learn new things. I love the feeling of opening 
                 a new blank page in VS Code and start working on a new feature that requires me to research and problem solve.</p>
                 <p className="py-6">When I am not coding I love adventure, be out in nature, explore new places or share a good meal or coffee with love ones.</p>
