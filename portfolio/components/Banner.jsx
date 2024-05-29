25caac80f5ef8d2f5f8fa49b3b6d93cc119601d5
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-29 20:33:27 +0200

    Update Banner.jsx
    Remove img class and change font size

--- portfolio/components/Banner.jsx
+++ portfolio/components/Banner.jsx
@@ -3,9 +3,9 @@ const Banner = () => {
   return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <Image width="600" height="700" src="/images/switzerland.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <Image width="600" height="700" src="/images/switzerland.jpg" className="rounded-lg shadow-2xl" />
                 <div>
-                <h1 className="text-5xl font-bold sm:text-3xl">&quot;Hello World!&quot;</h1>
+                <h1 className="text-5xl font-bold sm:text-xl">&quot;Hello World!&quot;</h1>
                 <p className="py-6">My name is Emanuel and I am a passionate coder who love to write logic and deepen my knowledge and learn new things. I love the feeling of opening 
                 a new blank page in VS Code and start working on a new feature that requires me to research and problem solve.</p>
                 <p className="py-6">When I am not coding I love adventure, be out in nature, explore new places or share a good meal or coffee with love ones.</p>
