e092033cfe72a8d95b0461024abe2856a20264ca
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-05-29 18:22:33 +0000

    change nav bg color to white

--- portfolio/components/Nav.jsx
+++ portfolio/components/Nav.jsx
@@ -3,24 +3,24 @@ import { FaLinkedin, FaGithub, FaEnvelope, FaFile } from 'react-icons/fa';
 
 const Nav = () => {
   return (
-    <div className='w-full h-auto fixed z-10'>
+    <div className='w-full h-auto fixed z-10 bg-white'>
     <div class="grid grid-cols-4 gap-4 content-start">
-            <button className="btn ">
+            <button className="btn bg-white">
             <Link href='https://github.com/EmanuelGustafzon' target='_blank' rel='noopener noreferrer'>
               <FaGithub className='text-2xl text-slate-900' />
             </Link>
           </button>
-          <button className="btn">
+          <button className="btn bg-white">
             <Link href='https://www.linkedin.com/in/emanuel-gustafzon-software-developer/' target='_blank' rel='noopener noreferrer'>
               <FaLinkedin className='text-2xl text-sky-700' />
             </Link>
           </button>
-          <button className="btn join-item">
+          <button className="btn bg-white">
             <Link href='' target='_blank' rel='noopener noreferrer'>
               <FaEnvelope className='text-2xl text-red-600' />
             </Link>
           </button>
-          <button className="btn join-item">
+          <button className="btn bg-white">
             <Link href='' target='_blank' rel='noopener noreferrer'>
               <FaFile className='text-2xl text-green-600' />
             </Link>
