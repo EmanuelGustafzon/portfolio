9c3aadb988ecec3649c7e922dc4eabe0a9f6177b
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-30 12:11:40 +0200

    Update Card.jsx
    Change h2 to h3

--- portfolio/components/Card.jsx
+++ portfolio/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({title, content, website, sourceCode}) => {
   return (
     <div className="card w-96 bg-neutral text-neutral-content">
       <div className="card-body items-center text-center">
-        <h3 className="card-title">{title}</h2>
+        <h3 className="card-title">{title}</h3>
         <p>{content}</p>
         <div className="card-actions justify-end">
           <Link href={website} className="btn text-gray-100">Website</Link>
