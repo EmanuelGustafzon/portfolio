aac972be5c2ad03301adb3ff5ac013f657fa4554
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-30 12:21:30 +0200

    Update Card.jsx
    Change color

--- portfolio/components/Card.jsx
+++ portfolio/components/Card.jsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 const Card = ({title, content, website, sourceCode}) => {
   return (
-    <div className="card w-96 bg-neutral text-neutral-content">
+    <div className="card w-96 bg-gray-300 text-white">
       <div className="card-body items-center text-center">
         <h3 className="card-title">{title}</h3>
         <p>{content}</p>
         <div className="card-actions justify-end">
           <Link href={website} className="btn text-gray-100">Website</Link>
-          <Link href={sourceCode} className="btn text-gray-100">Source Code</Link>
+          <Link href={sourceCode} className="btn text-white bg-gray-100">Source Code</Link>
         </div>
   </div>
 </div>
