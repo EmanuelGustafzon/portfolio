e2ab7e6992cd2ffd04df91c014fc4b9fb60f28f9
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-05-30 16:22:24 +0000

    change links

--- portfolio/components/Nav.jsx
+++ portfolio/components/Nav.jsx
@@ -5,25 +5,19 @@ const Nav = () => {
   return (
     <div className='w-full h-auto fixed z-10 bg-gray-100'>
       <div class="grid grid-cols-4 gap-4 content-start">
-              <button className="btn bg-gray-100 border-none">
-              <Link href='https://github.com/EmanuelGustafzon' target='_blank' rel='noopener noreferrer'>
+              <Link className="btn bg-gray-100 border-none" href='https://github.com/EmanuelGustafzon' target='_blank' rel='noopener noreferrer'>
                 <FaGithub className='text-2xl text-slate-900' />
               </Link>
-            </button>
-            <button className="btn bg-gray-100 border-none">
-              <Link href='https://www.linkedin.com/in/emanuel-gustafzon-software-developer/' rel='noopener noreferrer'>
+              <Link  className="btn bg-gray-100 border-none" href='https://www.linkedin.com/in/emanuel-gustafzon-software-developer/' target='_blank' rel='noopener noreferrer'>
                 <FaLinkedin className='text-2xl text-sky-700' />
               </Link>
-            </button>
-            <button className="btn bg-gray-100 border-none">
-              <a href='mailto:emanuel.gustafzon@gmail.com' target='_blank' rel='noopener noreferrer'>
+              <a className="btn bg-gray-100 border-none" href='mailto:emanuel.gustafzon@gmail.com' target='_blank' rel='noopener noreferrer'>
                 <FaEnvelope className='text-2xl text-red-600' />
               </a>
-            </button>
             <button className="btn bg-gray-100 border-none">
-              <Link href='resume.pdf' target='_blank' download rel='noopener noreferrer'>
+              <a href='resume.pdf' target='_blank' download rel='noopener noreferrer'>
                 <FaFile className='text-2xl text-green-600' />
-              </Link>
+              </a>
               CV
             </button>
         </div>
