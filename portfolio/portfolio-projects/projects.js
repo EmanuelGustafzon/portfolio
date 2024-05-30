bfdc7c27edbe8ddbbfc64be8bfcdc3e06a20d457
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-30 12:08:53 +0200

    Update projects.js
    Change name and remove image fiels

--- portfolio/portfolio-projects/projects.js
+++ portfolio/portfolio-projects/projects.js
@@ -3,23 +3,21 @@ export const projects = [
       id: 1,
       title: 'Declarative List',
       content: 'I wanted to build a unique project that also enhance my functional programming skills, building data structures and algorhitms and designing my own package. This is a library for working with linked lists according to functional programming principles.',
-      deployed: 'https://emanuelgustafzon.github.io/Milestone-project-1-music-lessons',
+      website: 'https://emanuelgustafzon.github.io/Milestone-project-1-music-lessons',
       sourceCode: 'https://github.com/EmanuelGustafzon/Milestone-project-1-music-lessons'
     },
     {
       id: 2,
       title: 'Learn Spanish Quiz',
-      image: '/images/quiz.png',
       content: 'A quiz game for people who wants to learn new phrases and words in Spanish built with JavaScript, HTML, and CSS.',
-      deployed: 'https://emanuelgustafzon.github.io/Milestone-project-2-Quiz/',
+      website: 'https://emanuelgustafzon.github.io/Milestone-project-2-Quiz/',
       sourceCode: 'https://github.com/EmanuelGustafzon/Milestone-project-2-Quiz',
     },
     {
       id: 3,
       title: 'Escape The Building',
-      image: '/images/escape.png',
       content: 'Text Based adventure game that guides you through a story and you need to make different decisions along the way. The application is built with Python',
-      deployed: 'https://escape-the-building.herokuapp.com/',
+      website: 'https://escape-the-building.herokuapp.com/',
       sourceCode: 'https://github.com/EmanuelGustafzon/escape-the-building',
     },
   ]
