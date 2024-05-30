4687f1c59d632377fa73d75dad2a2f6125cf6b0c
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-30 16:33:03 +0200

    Update projects.js
    Update links

--- portfolio/portfolio-projects/projects.js
+++ portfolio/portfolio-projects/projects.js
@@ -3,21 +3,19 @@ export const projects = [
       id: 1,
       title: 'Declarative List',
       content: 'I wanted to build a unique project that also enhance my functional programming skills, building data structures and algorhitms and designing my own package. This is a library for working with linked lists according to functional programming principles.',
-      website: 'https://emanuelgustafzon.github.io/Milestone-project-1-music-lessons',
-      sourceCode: 'https://github.com/EmanuelGustafzon/Milestone-project-1-music-lessons'
+      sourceCode: 'https://github.com/EmanuelGustafzon/declarative-linked-list'
     },
     {
       id: 2,
-      title: 'Dotnet Web API Recipe App',
+      title: 'Food Planner API in C#',
       content: 'After learning the fundamentals of C# I was enthusiastic about dotnet, Azure, SQL Server and Entity Framework and I built this project where users can add and manage Recipes, create a shopping list and populate the items from a recipe directly to the shopping list.',
-      website: 'https://emanuelgustafzon.github.io/Milestone-project-2-Quiz/',
-      sourceCode: 'https://github.com/EmanuelGustafzon/Milestone-project-2-Quiz',
+      sourceCode: 'https://github.com/EmanuelGustafzon/FoodPlannerAPI',
     },
     {
       id: 3,
       title: 'Embracing Experience',
       content: 'I built a content management system in Next.JS and MongoDB where I can login and manage blog posts with images, videos and text and even a quiz and google maps',
-      website: 'https://escape-the-building.herokuapp.com/',
-      sourceCode: 'https://github.com/EmanuelGustafzon/escape-the-building',
+      website: 'https://embracingexperiences.com/',
+      sourceCode: 'https://github.com/EmanuelGustafzon/embracing-experiences',
     },
   ]
